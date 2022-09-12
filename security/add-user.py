46ffddbff1fa4936fcea3eb5055985eb2061ffb5
Yuval Shaul <yuval.shaul@gmail.com>
2022-09-12 04:27:48 -0400

    saving files

--- security/add-user.py
+++ security/add-user.py
@@ -2,13 +2,12 @@ import boto3
 import json
 
 
-def demo_boto3():
+def add_my_user(user_name):
     client = boto3.client('iam', aws_access_key_id='AKIAVRHR5XR6NMPDRE2I',
                                       aws_secret_access_key='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',
                                       region_name='us-east-1')
-    users = client.list_users()
-    clean_users = users['Users']
-    print(clean_users)
+    response = client.create_user(UserName = user_name)
+    print(response)
 
 
-demo_boto3()
+add_my_user('new-user')
