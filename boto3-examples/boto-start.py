36432a540f965f390610b38986e431ca1fad505d
Yuval Shaul <yuval.shaul@gmail.com>
2023-01-23 09:29:18 -0500

    adding boto start example

--- boto3-examples/boto-start.py
+++ boto3-examples/boto-start.py
@@ -3,15 +3,16 @@ import boto3
 import json
 
 
-def read_my_credentials(credfile="cred.json"):
-    cf = open(os.getcwd() + '/aws/' + credfile, 'r')
+def read_my_credentials(credfile="creds.json"):
+    local_place = os.getcwd()
+    cf = open( local_place + '/aws/boto3-examples/' + credfile, 'r')
     creds = json.load(cf)
     return creds
 
 def demo_boto3():
     creds = read_my_credentials()
-    client = boto3.client('sts', aws_access_key_id=creds['access-key-id'],
-                                      aws_secret_access_key=creds['secret-access-key'],
+    client = boto3.client('sts', aws_access_key_id=creds['access_key_id'],
+                                      aws_secret_access_key=creds['secret_access_key'],
                                       region_name=creds['region'])
     print(client.get_caller_identity())
 
