75d034c81d5f740eaf2823727d3f3ebfe6c637da
Yuval Shaul <yuval.shaul@gmail.com>
2023-01-25 06:29:32 -0500

    updating all_instances.py

--- aws-sdks/boto3-examples/all_instances.py
+++ aws-sdks/boto3-examples/all_instances.py
@@ -2,14 +2,8 @@ import boto3
 import os
 import json
 
-def read_my_credentials(credfile="creds.json"):
-    local_place = os.getcwd()
-    cf = open( local_place + '/aws/boto3-examples/' + credfile, 'r')
-    creds = json.load(cf)
-    return creds
 
 def get_all_instances_iterator():
-    creds = read_my_credentials()
     ec2_resource = boto3.resource('ec2')
     instances_iterator = ec2_resource.instances.all()
     
@@ -17,7 +11,6 @@ def get_all_instances_iterator():
 
 
 def demo_ec2():
-    ec2_client = boto3.client('ec2')
     instances_iterator = get_all_instances_iterator()
     for inst in instances_iterator:
         print(inst.id, inst.state["Name"])
